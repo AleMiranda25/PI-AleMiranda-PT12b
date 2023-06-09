13aa7df2673768d7934988848a7210353ef8202f
Ale Miranda <alemiranda2511@gmail.com>
2023-06-09 20:18:07 -0300

    Carga de PI

---
+++ Client/src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.Presentacion}>
+      <h1 align="center">¡Hola, soy Ale Miranda!</h1>
+      <h1 align="center">Alumno de la Cohorte PT12b de Henry</h1>
+      <h1 align="center">Les presento mi humilde proyecto de Rick & Morty</h1>
+    </div>
+  );
+};
+
+export default About;
